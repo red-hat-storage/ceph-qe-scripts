98a0f1e08fda4adf78a690a7bc56b3e916f1edce
Shilpa <shilpa@dhcp35-184.lab.eng.blr.redhat.com>
2016-01-08 19:17:57 +0530

    Modified glance library

--- openstack/regression/lib/glance.py
+++ openstack/regression/lib/glance.py
@@ -1,84 +1,162 @@
 import os
-import urllib
 import keystoneclient.v2_0.client as ksclient
 import glanceclient.v2.client as glclient
-import config as cfg
-import random
+import keystoneclient.exceptions as ks_exceptions
+import glanceclient.common.exceptions as gl_exceptions
 import log
+from utils.image import download_image
 
-# Download a test image to temporary location and upload the image to glance
 
-
-class Glance(object):
+class GlanceReturnStack(object):
 
     def __init__(self):
-        keystone = {}
-        keystone['username'] = os.environ['OS_USERNAME']
-        keystone['password'] = os.environ['OS_PASSWORD']
-        keystone['auth_url'] = os.environ['OS_AUTH_URL']
-        keystone['tenant_name'] = os.environ['OS_TENANT_NAME']
-        ks_creds = ksclient.Client(**keystone)
-        glance_endpoint = ks_creds.service_catalog.url_for(service_type='image')
-        self.glance = glclient.Client(glance_endpoint, token=ks_creds.auth_token)
+        pass
 
 
-# Upload an image to glance from the given location
+class GlanceAuth(object):
 
-    def upload_images(self):
+    def __init__(self):
+
+        self.keystone = {}
+        self.keystone['username'] = os.environ['OS_USERNAME']
+        self.keystone['password'] = os.environ['OS_PASSWORD']
+        self.keystone['auth_url'] = os.environ['OS_AUTH_URL']
+        self.keystone['tenant_name'] = os.environ['OS_TENANT_NAME']
+        self.ks_creds = ksclient.Client(**self.keystone)
+        self.glance_endpoint = self.ks_creds.service_catalog.url_for(service_type='image')
+
+    def auth(self):
+
+        """
+
+        :return: :auth_stack
+                - auth_stack.glance : glance object after authenticating
+                - auth_stack.status : True or False
+        """
+
+        auth_stack = GlanceReturnStack()
+
+        log.info('Authenticating glance')
+
+        try:
+            glance = glclient.Client(self.glance_endpoint, token=self.ks_creds.auth_token)
+            auth_stack.glance, auth_stack.status = glance, True
+            log.info("Glance auth successful")
+
+        except ks_exceptions.AuthorizationFailure, e:
+            auth_stack.glance, auth_stack.status = None, False
+            log.error('Glance auth failed')
+            log.error(e.message)
+
+        return auth_stack
+
+
+class GlanceActions(object):
+
+    """ Upload an image to glance from the given location """
+
+    def __init__(self, glance_auth):
+        self.glance = glance_auth
+
+    def upload_images(self, name):
+
+        """
+        :param name: string
+        :return: upload_images.image : image object
+                 upload_images.status: True or False
+        """
+
         log.info("Uploading image to glance")
-        urllib.urlretrieve(cfg.image_loc, cfg.download_loc)
-        fimage = cfg.download_loc
-        if not fimage:
-            log.error("Image not found")
-            return False
-        else:
-            log.info("Creating glance image")
-            name = "test-img-" + str(random.randint(1, 20))
-            image = self.glance.images.create(name=name, disk_format="raw", container_format="bare")
-            try:
-                self.glance.images.upload(image.id, open(fimage, 'rb'))
-            except Exception as e:
-                log.error("Cannot upload glance image")
-                log.error(e)
-            image_name = image.name
-            log.debug("Image uploaded %s" % image_name)
-            return image_name
+
+        glance_create = GlanceReturnStack()
+
+        fimage = download_image()
+
+        try:
+            log.info('initialized image creation')
+            img = self.glance.images.create(name=name, disk_format="raw", container_format="bare")
+            glance_image = self.glance.images.upload(img.id, open(fimage, 'rb'))
+            glance_create.image, glance_create.status = glance_image, True
+
+        except gl_exceptions.ClientException as e:
+            log.error(e)
+            glance_create.image, glance_create.status = None, False
+
+        return glance_create
 
 
-# List images
+    def list_images(self):
 
-    def list_image(self):
-        images = self.glance.images.list()
-        if not images:
-            log.debug("No images to list")
-        else:
-            return list(images)
 
-# Get image by id
+        """
+       :return volumes_list
+                - image_list.images : list of image objects
+                - image_list.status  : True or False
+        """
 
-    def get_image(self, image_name):
-        image_list = self.glance.images.list()
-        for img in image_list:
-            if img.name == image_name:
-                return img.id
+        image_list = GlanceReturnStack()
+        image_list.images = []
 
-# Get image by name
+        try:
+            log.info("Listing images")
+            images = self.glance.images.list()
 
-    def get_image_by_name(self,image_id):
-        image_list = self.glance.images.list()
-        for img in image_list:
-            if img.id == image_id:
-                return img.name
+            image_list.status = True
+            if images:
+                image_list.images = images
 
-# Delete image
+        except (gl_exceptions.ClientException, gl_exceptions.NotFound), e:
+            log.error(e)
+            image_list.status = False
 
-    def delete_image(self, image_name):
-        img = self.get_image(image_name)
-        img_del = self.glance.images.delete(img)
-        if img_del is None:
-            log.info("Deleted the image %s successfully" % img_del)
-        else:
-            log.debug("Cannot delete image %s" % img_del)
+        return image_list
+
+    def get_image(self, image):
+
+        """
+        :param image: volume object
+        :return:each_image
+                - each_image.image : image object
+                - each_image.status : True or False
+
+        """
+
+        each_image = GlanceReturnStack()
+        each_image.image = None
+
+        try:
+            log.info('Get image attributes')
+            image = self.glance.images.get(image.id)
+            each_image.image = image
+            each_image.status = True
+
+        except (gl_exceptions.ClientException, gl_exceptions.NotFound), e:
+            log.error(e)
+            each_image.status = False
+
+        return each_image
+
+    def delete_image(self, image):
+
+        """
+        :param image: image object
+        :return: image_delete
+                 - image_delete.execute: True or False
+        """
+
+        log.info('Deleting image')
+        image_delete = GlanceReturnStack()
+        image_delete.execute = False
+
+        try:
+            self.glance.images.delete(image)
+            image_delete.execute = True
+            log.info('delete volume executed')
+        except (gl_exceptions.NotFound, gl_exceptions.ClientException), e:
+            log.error(e)
+            image_delete.execute = False
+
+        return image_delete
 
 
 
