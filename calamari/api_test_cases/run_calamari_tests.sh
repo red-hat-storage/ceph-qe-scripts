02973f2fa5ac890082f91106fd7238adfb17c45c
Vasu Kulkarni <vasu@redhat.com>
2016-07-20 16:17:24 -0700

    Use default values when running calamari api tests
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- calamari/api_test_cases/run_calamari_tests.sh
+++ calamari/api_test_cases/run_calamari_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # example :
 # sh run_calamari_tests.sh -http=https -ip=10.8.128.63 -port=8002 -u=admin -p=admin123 -l=/tmp/log
+set -x
 
 for i in "$@"
 do
@@ -31,6 +32,26 @@ echo PORT = ${PORT}
 echo USERNAME = ${UNAME}
 echo PASSWORD = ${PASSWD}
 echo LOG_PATH = ${LOG}
+
+[ -z $HTTP ] && HTTP=https
+[ -z $IP ] && IP=localhost
+[ -z $PORT ] && PORT=8002
+[ -z $UNAME ] && UNAME=admin
+[ -z $PASSWD ] && PASSWD=admin
+[ -z $LOG ] && LOG="/tmp/apilog"
+
+echo protocol = ${HTTP}
+echo IP = ${IP}
+echo PORT = ${PORT}
+echo USERNAME = ${UNAME}
+echo PASSWORD = ${PASSWD}
+echo LOG_PATH = ${LOG}
+
+virtualenv venv
+source venv/bin/activate
+pip install pyyaml
+pip install requests
+pip install names
 
 python generate_config.py -http ${HTTP} -ip ${IP} -port ${PORT} -u ${UNAME} -p ${PASSWD} -log ${LOG}
 python codify.py
