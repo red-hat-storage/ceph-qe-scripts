c101fe5a89a3f1bdd3c7bf5f98ed0bed279bebcc
rakeshgm <rgowdege@redhat.com>
2016-11-02 15:13:18 +0530

    renamed of a file and added assert to Mbuckets test.

--- rgw/tests/s3/t.py
+++ rgw/tests/s3/t.py
@@ -4,39 +4,67 @@ import boto.s3.connection
 import sys
 from boto.s3.key import Key
 from random import randint
+import subprocess
+# import utils.log as log
+import json
 
+
+class RGWAdminOps(object):
+
+    def __init__(self):
+
+        self.exec_cmd = lambda cmd: subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+
+    def create_admin_user(self, username, displayname):
+
+        try:
+            cmd = 'radosgw-admin user create --uid="%s" --display-name="%s"' % (username, displayname)
+            # log.info('cmd')
+            variable = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+            v = variable.stdout.read()
+
+            v_as_json = json.loads(v)
+
+            # log.info(v_as_json)
+
+            user_details = {}
+
+            user_details['user_id'] = v_as_json['user_id']
+            user_details['display_name'] = v_as_json['display_name']
+
+            user_details['access_key'] = v_as_json['keys'][0]['access_key']
+
+            user_details['secret_key'] = v_as_json['keys'][0]['secret_key']
+
+            return user_details
+
+        except subprocess.CalledProcessError as e:
+            error = e.output + str(e.returncode)
+            # og.error(error)
+            return False
 
 u1_bucket = 'sandy2.bucky.0'
 u2_bucket = 'margaret2.bucky.0'
 
 
-access_key = 'ORQ0E8NCG6G19D46KJA4'
-secret_key = 'c02xYBMXUrcPETCtTZOTXBO24dATp8naiF4GrZsF'
+#ua = RGWAdminOps()
+#user_details = ua.create_admin_user('rakesh', 'rakeshgm')
+
+
+access_key = "9I61W6PMDXX8FOBEKOCE"
+secret_key = "LMw7MciduMP6V4b7EjYm9T7al5UKkcXjavGF9J9L"
 
 conn = boto.connect_s3(
     aws_access_key_id=access_key,
     aws_secret_access_key=secret_key,
-    host='magna118',
-    port=8080,
-    is_secure=False,
-    calling_format=boto.s3.connection.OrdinaryCallingFormat(),
-)
-
-access_key2 = 'E7P4AFVG7ML4FS880QDW'
-secret_key2 = 'KX45ecyo9hdxub7JhfrwgiNz6MOdKD29B9WPoTj7'
-
-conn2 = boto.connect_s3(
-    aws_access_key_id=access_key2,
-    aws_secret_access_key=secret_key2,
-    host='magna118',
+    host='magna022',
     port=8080,
     is_secure=False,
     calling_format=boto.s3.connection.OrdinaryCallingFormat(),
 )
 
 
-can_id = conn2.get_canonical_user_id()
-print can_id
+conn.get_canonical_user_id()
 
 bu = conn.get_bucket(u1_bucket)
 
@@ -47,9 +75,10 @@ for grant in acp.acl.grants:
     print grant.permission, grant.id
 
 
+
+"""
+
 all_keys = bu.get_all_keys()
-
-
 for key in all_keys:
     print 'all keys 1--------------'
     print 'name: %s' % key.name
@@ -59,32 +88,4 @@ for key in all_keys:
     print 'md5: %s' % key.md5
 
     print '--------------'
-
-
-bu2 = conn.get_bucket(u2_bucket)
-
-acp2 = bu2.get_acl()
-
-for grant in acp2.acl.grants:
-    print grant.permission, grant.id
-
-print 'copying the objects from u1 to u2'
-
-for each in all_keys:
-    bu2.copy_key(each.key, bu.name, each.key)
-
-all_keys2 = bu2.get_all_keys()
-
-for key in all_keys2:
-    print 'all keys 2--------------'
-    print 'name: %s' % key.name
-    print 'version_id %s' % key.version_id
-    print 'size: %s' % key.size
-    print 'etag: %s' % key.etag
-    print 'md5: %s' % key.md5
-
-    print '--------------'
-
-
-
-
+"""
