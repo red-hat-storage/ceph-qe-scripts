cf96961de7e606218d56225c9b27458f225ac63e
rakeshgm <rgowdege@redhat.com>
2017-11-27 16:27:57 +0530

    nfs ganesha tests now uses systemctl

--- rgw/lib/nfs_ganesha/manage_services.py
+++ rgw/lib/nfs_ganesha/manage_services.py
@@ -16,29 +16,45 @@ class ManageNFSServices(object):
 
         log.info('starting nfs-ganesha services')
 
-        cmd = 'sudo /usr/bin/ganesha.nfsd -f /etc/ganesha/ganesha.conf'
+        # cmd = 'sudo /usr/bin/ganesha.nfsd -f /etc/ganesha/ganesha.conf'
+        # utils.exec_shell_cmd(cmd)
+
+        cmd = 'sudo systemctl enable nfs-ganesha '
+        utils.exec_shell_cmd(cmd)
+
+        cmd = 'sudo systemctl start nfs-ganesha '
         utils.exec_shell_cmd(cmd)
 
     def ganesha_stop(self):
 
-        log.info('stopping ganesha services')
+        log.info('stopping ganesha services via systemctl')
 
         # for now there is no way to stop ganesha services, only option is to kill the process
 
-        p = Process(name='ganesha')
-        p.find()
 
-        if p.process is None:
-            log.info('process nor running')
-        else:
-            p.process.kill()
+        # p = Process(name='ganesha')
+        # p.find()
+        #
+        # if p.process is None:
+        #     log.info('process nor running')
+        # else:
+        #     p.process.kill()
+        #
+
+        cmd = 'sudo systemctl stop nfs-ganesha'
+        utils.exec_shell_cmd(cmd)
 
     def ganesha_restart(self):
 
         log.info('restarting ganesha services')
 
-        self.ganesha_stop()
-        self.ganesha_start()
+        # self.ganesha_stop()
+        # self.ganesha_start()
+
+        log.info('restarting services using systemctl')
+
+        cmd = 'sudo systemctl restart nfs-ganesha'
+        utils.exec_shell_cmd(cmd)
 
     def kernel_stop(self):
 
