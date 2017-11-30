83187757a144eac8664476ee60f2b23f4f0a2a6f
shreekarSS <sshreeka@redhat.com>
2017-11-30 18:22:38 +0530

    new file:   __init__.py 	new file:   ceph_medic_cli_validation.py: This script will validates the cli's of ceph-medic. 	new file:   ceph_medic_error_code_validation.py: This script will simulate the errors and runs "ceph-medic check" which should detect the errors in the cluster which is generated.Later it reverts the generated errors. 	new file:   ceph_medic_install.py: This script installs ceph-medic by checking OS.

---
+++ ceph-medic/tests/ceph_medic_cli_validation.py
@@ -0,0 +1,72 @@
+import sys,os
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../")))
+import argparse
+import subprocess
+from utils.utils import *
+from utils  import log
+
+
+class CliValidationTest(object):
+
+    def __init__(self, cluster_name):
+
+        self.cluster_name = cluster_name
+
+        log.info('got cluster name : %s' % self.cluster_name)
+
+        self.check_op = lambda error_to_check, cmd_output: True if error_to_check in cmd_output else False
+
+    def cli_validation(self,cmd,err_to_check,err_desc):
+
+        try:
+            cmd_output = subprocess.check_output(cmd).strip('\n')
+
+            error_exixts = self.check_op(err_to_check, cmd_output)
+
+            if error_exixts:
+
+                log.info("Test case for %s has  passed "%(err_desc))
+
+            else:
+                log.error("Test case for %s has failed "%(err_desc))
+
+
+        except (subprocess.CalledProcessError)as e:
+            log.error(e)
+
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(description='Ceph medic automation')
+
+    parser.add_argument('-cluster', dest="cluster_name",
+                        help='Cluster name ',default='ceph')
+
+
+    args = parser.parse_args()
+
+
+    CLUSTER_NAME = args.cluster_name
+
+    cli_validation_op= CliValidationTest(CLUSTER_NAME)
+    test_desc= []
+    for key in invalid_cmd_err:
+        test_desc.append(key)
+
+    test_result=[
+
+        cli_validation_op.cli_validation(cmd=['ceph-medic', '--inventory', '{}'.format(invalid_inventory), '--cluster','{}'.format(CLUSTER_NAME), 'check'],err_to_check=invalid_cmd_err['Invalid Inventory']['Invalid inventory error'],err_desc=test_desc[0]),
+
+        cli_validation_op.cli_validation(cmd=['ceph-medic', '--ssh-config', '{}'.format(invalid_ssh), '--cluster', '%s' % CLUSTER_NAME,'--inventory', 'check'],err_to_check=invalid_cmd_err['Invalid SSH']['Invalid ssh error'],err_desc=test_desc[1]),
+
+        cli_validation_op.cli_validation(cmd=['ceph-medic', '--ssh-config', '{}'.format(invalid_ssh), '--cluster', '{}'.format(CLUSTER_NAME),'--inventory', '{}'.format(invalid_inventory), 'check'],err_to_check=invalid_cmd_err['Invalid Invenotory and SSH']['Invalid invenotory and ssh error'],err_desc=test_desc[2]),
+
+        cli_validation_op.cli_validation(cmd=['ceph-medic', '--cluster', '{}'.format(invalid_cluster), 'check'],err_to_check=ERRORS.common_errs['ECOM1']['error_code'],err_desc=ERRORS.common_errs['ECOM1']['error_code']),
+
+        cli_validation_op.cli_validation(cmd=['ceph-medic', '{}'.format(invalid_cmd)],err_to_check=invalid_cmd_err['Invalid Command']['Invalid command error'],err_desc=test_desc[3])
+]
+
+
+
