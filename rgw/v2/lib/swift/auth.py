8a67b9c076851d95d37e5171ef9ce475b4cc3c57
rakeshgm <rgowdege@redhat.com>
2018-05-29 14:29:42 +0530

    added swift auth lib

---
+++ rgw/v2/lib/swift/auth.py
@@ -0,0 +1,30 @@
+import swiftclient
+import socket
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../../")))
+import v2.utils.log as log
+import v2.utils.utils as utils
+
+
+class Auth(object):
+
+    def __init__(self, user_info):
+        self.secret_key = user_info['key']
+        self.hostname = socket.gethostname()
+        self.port = int(utils.get_radosgw_port_no())
+        self.is_secure = False
+        self.user_id = user_info['user_id']
+
+    def do_auth(self):
+        log.info('performing authentication usinf swift')
+
+        # user = 'tenant3$tuffy3:swift'
+        # key = 'm4NsRGjghOpUPX3OZZFeIYUylNjO22lMVDXATnNi' -- secret key
+
+        rgw = swiftclient.Connection(user=self.user_id,
+                                     key=self.secret_key,
+                                     authurl='http://%s:%s/auth' % (self.hostname, self.port),
+                                     )
+
+        return rgw
