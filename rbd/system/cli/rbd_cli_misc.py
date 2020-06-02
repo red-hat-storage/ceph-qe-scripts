1ed718b7829bae3379d6966e8b455fc6352c5685
Gopi Patta <gpatta@redhat.com>
2020-06-02 08:46:17 +0000

    rbd cephci suite fix for nautilus and py2 to py3 conversion

--- rbd/system/cli/rbd_cli_misc.py
+++ rbd/system/cli/rbd_cli_misc.py
@@ -9,7 +9,7 @@ import utils.log as log
 FAILED_COUNT = 0
 PASSED_COUNT = 0
 FAILED_COMMANDS = []
-
+PASSED_COMMANDS = []
 
 def exec_cmd(args):
     rc = cli.rbd.exec_cmd(args)
@@ -18,6 +18,7 @@ def exec_cmd(args):
         FAILED_COMMANDS.append(args)
     else:
         globals()['PASSED_COUNT'] += 1
+        PASSED_COMMANDS.append(args)
     return rc
 
 
@@ -28,12 +29,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     k_m = args.ec_pool_k_m
     cli = parameters.CliParams(k_m=k_m, num_rep_pool=1, num_data_pool=1 if k_m else 0)
+    iterator = 0
 
     # Simple Image Creation
     combinations = cli.generate_combinations('image_size')
-    combinations = filter(lambda val: cli.search_param_val('-s', val)
+    combinations = list(filter(lambda val: cli.search_param_val('-s', val)
                           .find('G') != -1,
-                          combinations)
+                          combinations))
     [exec_cmd('rbd create {} {} {}/img{}'.format(combinations[0], parameters.data_pool['arg'] +
                                                  ' ' + parameters.data_pool['val']['pool0'],
                                                  parameters.rep_pool['val']['pool0'], iterator))
@@ -52,6 +54,7 @@ if __name__ == "__main__":
          for param in combinations]
 
     # Snap Creation
+    iterator = 0
     exec_cmd('rbd snap create {}/img{}@snapimg'
              .format(parameters.rep_pool['val']['pool0'], iterator))
 
@@ -63,8 +66,8 @@ if __name__ == "__main__":
     # Add Lock
     exec_cmd('rbd lock add {}/img{} 007'.format(parameters.rep_pool['val']['pool0'], iterator))
 
-    [exec_cmd('rbd lock add --shared tag {}/img{} {}'
-              .format(parameters.rep_pool['val']['pool0'], iterator - 1, lock_id))
+    [exec_cmd('rbd lock add --shared lock-tag {}/img{} {}'
+              .format(parameters.rep_pool['val']['pool0'], iterator + 1, lock_id))
      for lock_id in range(0, 2)]
 
     # List Lock
@@ -78,9 +81,14 @@ if __name__ == "__main__":
             json_output = json.loads(
                 exec_cmd('rbd lock list {}/img{} --format=json'
                          .format(parameters.rep_pool['val']['pool0'], iterator)))
-            [exec_cmd('rbd lock remove {}/img{} {} {}'.
-             format(parameters.rep_pool['val']['pool0'], iterator, key, val['locker']))
-             for key, val in json_output.iteritems()]
+            if cli.ceph_version == 3:
+                [exec_cmd('rbd lock remove {}/img{} {} {}'
+                          .format(parameters.rep_pool['val']['pool0'], iterator, key, val['locker']))
+                 for key, val in json_output.items()]
+            else:
+                [exec_cmd('rbd lock remove {}/img{} {} {}'
+                          .format(parameters.rep_pool['val']['pool0'], iterator, lock['id'], lock['locker']))
+                 for lock in json_output]
 
     # Mapping Images to block-device
     iterator += 1
@@ -112,7 +120,10 @@ if __name__ == "__main__":
     log.info('Commands Failed: {}'.format(FAILED_COUNT))
 
     if FAILED_COUNT > 0:
+        log.info('Failed commands')
         [log.info(fc) for fc in FAILED_COMMANDS]
+        log.info('Passed commands')
+        [log.info(fc) for fc in PASSED_COMMANDS]
         exit(1)
 
     exit(0)
