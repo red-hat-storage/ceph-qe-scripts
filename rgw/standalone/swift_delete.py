81b48a2f92e05c39987361470a5c78493e1c9a61
udaysk23 <ukurundw@redhat.com>
2020-07-06 12:39:42 +0530

    fix indentation issue
    Signed-off-by: udaysk23 <ukurundw@redhat.com>

---
+++ rgw/standalone/swift_delete.py
@@ -0,0 +1,24 @@
+"""
+This script will delete specified number of objects from specified container.
+Install swiftclient package on machine to run this script.
+"""
+import swiftclient
+
+user = '<Swift user id>'
+key = '<Swift user key>'
+count=0
+conn = swiftclient.Connection(
+        user=user,
+        key=key,
+        authurl='http://<RGW hostname or IP>:8080/auth/1.0',
+)
+container_name = '<Container name>'
+
+for i in conn.get_container(container_name)[1]:
+    name = i['name']
+    conn.delete_object(container_name, name)
+    print(i['name'])
+    count +=1
+    if count == <Number of objects to delete>:
+        break;
+        
