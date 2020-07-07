6640e25c2e46f5dce6ca8318409834c68394561b
udaysk23 <ukurundw@redhat.com>
2020-07-07 11:58:05 +0530

    Added s3 list operation script
    Signed-off-by: udaysk23 <ukurundw@redhat.com>

---
+++ rgw/standalone/s3_list.py
@@ -0,0 +1,30 @@
+"""
+This script will list objects from specified bucket.
+Install boto package on machine to run this script.
+"""
+
+import boto.s3.connection
+import boto
+
+access_key = '<Access_Key>'
+secret_key = '<Secret_Key>'
+
+conn = boto.connect_s3(
+    aws_access_key_id = access_key,
+     aws_secret_access_key = secret_key,
+    host = '<RGW HOST Name or IP>',
+     port = 8080,
+    is_secure=False,	#Change it to True if RGW running using SSL
+
+    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    )
+
+bucket = conn.create_bucket('<Bucket Name>')
+
+print("Listing objects")
+for key in bucket.list():
+	print("{name}\t{size}".format(name = key.name, size = key.size))
+
+
+
+
