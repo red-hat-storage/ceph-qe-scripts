19c50ea5713557097eccb426f0d9e225b3a75370
MadhaviKasturi <mkasturi@redhat.com>
2020-11-20 03:38:19 -0500

    test_bucket_listing.py: changes to these files
    - adding the bucket_listing.py and associated yaml's to run-all.sh
    - change of the object size in yaml files
    - added a check to delete the local file once uploaded
    - fetching the current .asok file incase there are many .asok files listed due to crashes
    
    Signed-off-by: Madhavi Kasturi <mkasturi@redhat.com>

--- rgw/v2/tests/s3_swift/run-all.sh
+++ rgw/v2/tests/s3_swift/run-all.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 scripts=("python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuckets.yaml"
-         "python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuckets_with_Nobjects.yaml"
+	 "python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuckets_with_Nobjects.yaml"
          "python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuckets_with_Nobjects_download.yaml"
          "python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuckets_with_Nobjects_aws4.yaml"
          "python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuckets_with_Nobjects_compression.yaml"
@@ -25,7 +25,7 @@ scripts=("python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuck
          "python3 $DIR/test_versioning_with_objects.py -c $DIR/configs/test_versioning_objects_suspend_re-upload.yaml"
          "python3 $DIR/test_versioning_with_objects.py -c $DIR/configs/test_versioning_suspend.yaml"
          "python3 $DIR/test_bucket_lifecycle_config_ops.py -c $DIR/configs/test_bucket_lifecycle_config_disable.yaml"
-         "python3 $DIR/test_bucket_lifecycle_config_ops.py -c $DIR/configs/test_bucket_lifecycle_config_modify.yaml"
+	 "python3 $DIR/test_bucket_lifecycle_config_ops.py -c $DIR/configs/test_bucket_lifecycle_config_modify.yaml"
          "python3 $DIR/test_bucket_lifecycle_config_ops.py -c $DIR/configs/test_bucket_lifecycle_config_read.yaml"
          "python3 $DIR/test_bucket_lifecycle_config_ops.py -c $DIR/configs/test_bucket_lifecycle_config_versioning.yaml"
          "python3 $DIR/test_bucket_policy_ops.py -c $DIR/configs/test_bucket_policy_delete.yaml"
@@ -40,10 +40,16 @@ scripts=("python3 $DIR/test_Mbuckets_with_Nobjects.py -c $DIR/configs/test_Mbuck
          "python3 $DIR/test_frontends_with_ssl.py -c $DIR/configs/test_ssl_civetweb.yaml"
          "python3 $DIR/user_op_using_rest.py -c $DIR/configs/test_user_with_REST.yaml"
          "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_date.yaml"
-	       "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_multiple_rule_prefix_current_days.yaml"
-	       "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_rule_prefix_and_tag.yaml"
-	       "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_rule_prefix_non_current_days.yaml"
-	       "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_rule_delete_marker.yaml")
+	 "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_multiple_rule_prefix_current_days.yaml"
+	 "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_rule_prefix_and_tag.yaml"
+	 "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_rule_prefix_non_current_days.yaml"
+	 "python3 $DIR/test_bucket_lifecycle_object_expiration.py -c $DIR/configs/test_lc_rule_delete_marker.yaml"
+	 "python3 $DIR/test_bucket_listing.py -c $DIR/configs/test_bucket_listing_flat_ordered.yaml"
+         "python3 $DIR/test_bucket_listing.py -c $DIR/configs/test_bucket_listing_flat_unordered.yaml"
+         "python3 $DIR/test_bucket_listing.py -c $DIR/configs/test_bucket_listing_flat_ordered_versionsing.yaml"
+         "python3 $DIR/test_bucket_listing.py -c $DIR/configs/test_bucket_listing_pseudo_ordered.yaml"
+         "python3 $DIR/test_bucket_listing.py -c $DIR/configs/test_bucket_listing_pseudo_ordered_dir_only.yaml")
+
 
 
 for script in "${scripts[@]}"
