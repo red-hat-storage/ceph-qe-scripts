f0d13e6f44bd2eac9c5afd17868f08c9ec7fb7cc
ckulal <ckulal@redhat.com>
2022-11-09 14:03:48 +0530

    Support s3cmd testcases run from client node
    Signed-off-by: ckulal <ckulal@redhat.com>

--- rgw/v2/tests/s3cmd/test_rate_limit.py
+++ rgw/v2/tests/s3cmd/test_rate_limit.py
@@ -45,7 +45,7 @@ log = logging.getLogger()
 TEST_DATA_PATH = None
 
 
-def test_exec(config):
+def test_exec(config, ssh_con):
     """
     Executes test based on configuration passed
     Args:
@@ -57,7 +57,7 @@ def test_exec(config):
     user_info = resource_op.create_users(no_of_users_to_create=config.user_count)[0]
     user_name = user_info["user_id"]
 
-    ip_and_port = s3cmd_reusable.get_rgw_ip_and_port()
+    ip_and_port = s3cmd_reusable.get_rgw_ip_and_port(ssh_con)
     s3_auth.do_auth(user_info, ip_and_port)
     # add rate limit capability to rgw user
     caps_add = utils.exec_shell_cmd(
@@ -194,13 +194,20 @@ if __name__ == "__main__":
             help="Set Log Level [DEBUG, INFO, WARNING, ERROR, CRITICAL]",
             default="info",
         )
+        parser.add_argument(
+            "--rgw-node", dest="rgw_node", help="RGW Node", default="127.0.0.1"
+        )
         args = parser.parse_args()
         yaml_file = args.config
+        rgw_node = args.rgw_node
+        ssh_con = None
+        if rgw_node != "127.0.0.1":
+            ssh_con = utils.connect_remote(rgw_node)
         log_f_name = os.path.basename(os.path.splitext(yaml_file)[0])
         configure_logging(f_name=log_f_name, set_level=args.log_level.upper())
         config = resource_op.Config(yaml_file)
         config.read()
-        test_exec(config)
+        test_exec(config, ssh_con)
         test_info.success_status("test passed")
         sys.exit(0)
 
