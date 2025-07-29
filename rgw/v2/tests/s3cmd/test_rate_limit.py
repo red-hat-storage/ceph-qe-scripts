257cb48a61d07aaca762ec90388ef430d33e5fb0
chaithra <ckulal@redhat.com>
2025-07-29 15:12:11 +0530

    [Closed-loop]: Bucket link operations fails for a bucket with rate limit set
    Signed-off-by: chaithra <ckulal@redhat.com>

--- rgw/v2/tests/s3cmd/test_rate_limit.py
+++ rgw/v2/tests/s3cmd/test_rate_limit.py
@@ -8,6 +8,8 @@ Usage: test_rate_limit.py -c <input_yaml>
     test_rate_limit.yaml
     test_ratelimit_split.yaml
     test_ratelimit_debt.yaml
+    test_bucket_link_with_ratelimit.yaml
+    test_user_link_with_ratelimit.yaml
 
 Polarion Tests:
 CEPH-83574910
@@ -20,6 +22,10 @@ Operation:
     Enable the limits max-read-ops, max-read-bytes, max-write-ops, max-write-bytes on a Bucket scope
     Verify the rate limits using s3cmd
     Repeat the same limits on a User scope
+
+    Test Bucket link ops: on bucket where rate limit config enabled
+    1. Bucket rename
+    2. Bucket owner change
 """
 
 import argparse
@@ -125,115 +131,163 @@ def test_exec(config, ssh_con):
     )
     log.info(f"Rate limits enabled on bucket : {limget} ")
 
-    # test the read and write ops limit
-    log.info(f"Test the read and write ops limits")
-    s3cmd_reusable.rate_limit_read(bucket_name, max_read_ops, ssl)
-
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-
-    s3cmd_reusable.rate_limit_write(bucket_name, max_write_ops, ssl)
-
-    # sleep a minute to reset the ops limit
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-
-    # test the read and write data limit
-    log.info(f"Test the read and write data limits")
-    s3cmd_reusable.rate_limit_read(bucket_name, max_read_bytes_kb, ssl)
-
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-    s3cmd_reusable.rate_limit_write(bucket_name, max_write_bytes_kb, ssl)
-
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-
-    # Set the rate limits for the user and enable them
-    max_read_bytes = config.user_max_read_bytes
-    max_read_ops = config.user_max_read_ops
-    max_write_bytes = config.user_max_write_bytes
-    max_write_ops = config.user_max_write_ops
-
-    max_read_bytes_kb = math.ceil(float(max_read_bytes) / 1024)
-    max_write_bytes_kb = math.ceil(float(max_write_bytes) / 1024)
-
-    utils.exec_shell_cmd(
-        f"radosgw-admin ratelimit disable --ratelimit-scope=bucket --bucket={bucket_name}"
-    )
-    limset = utils.exec_shell_cmd(
-        f"radosgw-admin ratelimit set --ratelimit-scope=user --uid={user_name}"
-        + f" --max-read-ops={max_read_ops} --max-read-bytes={max_read_bytes}"
-        + f" --max-write-bytes={max_write_bytes} --max-write-ops={max_write_ops}"
-    )
-    log.info(f"Rate limits set on user {user_name}")
-    limenable = utils.exec_shell_cmd(
-        f"radosgw-admin ratelimit enable --ratelimit-scope=user --uid={user_name}"
-    )
-    limget = utils.exec_shell_cmd(
-        f"radosgw-admin ratelimit get --ratelimit-scope=user --uid={user_name}"
-    )
-    log.info(f"Rate limits enabled on bucket : {limget} ")
-
-    bucket_name2 = utils.gen_bucket_name_from_userid(user_name, rand_no=1)
-    bucket_name2 = bucket_name2.replace("'", "")
-    if config.version_enable:
-        s3cmd_reusable.create_versioned_bucket(
-            user_info, bucket_name2, ip_and_port, ssl
+    if config.test_ops.get("test_bucket_link_with_ratelimit", False):
+        log.info("Testing bucket link ops with ratelimit set")
+        log.info(
+            "Perform bucket rename operation using radosgw-admin bucket link command"
         )
+        bucket_name_new = f"{bucket_name}-new"
+        cmd = f"radosgw-admin bucket link --bucket={bucket_name} --bucket-new-name={bucket_name_new} --uid={user_name}"
+        output = utils.exec_shell_cmd(cmd, return_err=True)
+        log.info(f"output is {output}")
+        if (
+            "(22) Invalid argument: While crossing the Anavros you have displeased the goddess Hera"
+            in output
+        ):
+            raise AssertionError(
+                f"Bucket rename with bucket link command failed for ratelimit enabled bucket {bucket_name}"
+            )
+        out = utils.exec_shell_cmd(
+            f"radosgw-admin bucket stats --bucket {bucket_name_new}"
+        )
+
+    elif config.test_ops.get("test_user_link_with_ratelimit", False):
+        log.info("Testing user link ops with ratelimit set")
+        existing_user_name = user_name
+        new_user_name = f"{existing_user_name}-new"
+        utils.exec_shell_cmd(
+            f"radosgw-admin user create --uid {new_user_name} --display-name {new_user_name}"
+        )
+        log.info(f"unlink bucket {bucket_name} from existing user {existing_user_name}")
+        utils.exec_shell_cmd(
+            f"radosgw-admin bucket unlink --bucket {bucket_name} --uid {existing_user_name}"
+        )
+        log.info(f"Perform new user:{new_user_name} link to the bucket {bucket_name}")
+        cmd = f"radosgw-admin bucket link --bucket={bucket_name} --uid={new_user_name}"
+        output = utils.exec_shell_cmd(cmd, return_err=True)
+        log.info(f"output is {output}")
+        if (
+            "(22) Invalid argument: While crossing the Anavros you have displeased the goddess Hera"
+            in output
+        ):
+            raise AssertionError(
+                f"Bucket link to new user failed for ratelimit enabled bucket {bucket_name}"
+            )
+        out = json.loads(
+            utils.exec_shell_cmd(f"radosgw-admin bucket stats --bucket {bucket_name}")
+        )
+        if out["owner"] != new_user_name:
+            raise AssertionError("Bucket owner change failed!")
     else:
-        s3cmd_reusable.create_bucket(bucket_name2, ssl)
-
-    # test the read and write ops limit
-    log.info(f"Test the read and write ops limits")
-    s3cmd_reusable.rate_limit_read(bucket_name2, max_read_ops, ssl)
-
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-
-    s3cmd_reusable.rate_limit_write(bucket_name2, max_write_ops, ssl)
-
-    # sleep a minute to reset the ops limit
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-
-    # test the read and write data limit
-    log.info(f"Test the read and write data limits")
-    s3cmd_reusable.rate_limit_read(bucket_name2, max_read_bytes_kb, ssl)
-
-    log.info(f"Sleeping for a minute to reset limits")
-    sleep(61)
-    s3cmd_reusable.rate_limit_write(bucket_name2, max_write_bytes_kb, ssl)
-
-    if config.test_ops.get("daemon_add", False):
-        log.info("Add a RGW daemon to the existing configuration")
-        s3cmd_reusable.rgw_daemon_add(ssh_con)
-        log.info("Verify that the ratelimits should now be halved for each daemon")
-        max_read_ops = math.ceil(max_read_ops / 2)
-        max_write_ops = math.ceil(max_write_ops / 2)
         # test the read and write ops limit
-        sleep(60)
+        log.info(f"Test the read and write ops limits")
+        s3cmd_reusable.rate_limit_read(bucket_name, max_read_ops, ssl)
+
+        log.info(f"Sleeping for a minute to reset limits")
+        sleep(61)
+
+        s3cmd_reusable.rate_limit_write(bucket_name, max_write_ops, ssl)
+
+        # sleep a minute to reset the ops limit
+        log.info(f"Sleeping for a minute to reset limits")
+        sleep(61)
+
+        # test the read and write data limit
+        log.info(f"Test the read and write data limits")
+        s3cmd_reusable.rate_limit_read(bucket_name, max_read_bytes_kb, ssl)
+
+        log.info(f"Sleeping for a minute to reset limits")
+        sleep(61)
+        s3cmd_reusable.rate_limit_write(bucket_name, max_write_bytes_kb, ssl)
+
+        log.info(f"Sleeping for a minute to reset limits")
+        sleep(61)
+
+        # Set the rate limits for the user and enable them
+        max_read_bytes = config.user_max_read_bytes
+        max_read_ops = config.user_max_read_ops
+        max_write_bytes = config.user_max_write_bytes
+        max_write_ops = config.user_max_write_ops
+
+        max_read_bytes_kb = math.ceil(float(max_read_bytes) / 1024)
+        max_write_bytes_kb = math.ceil(float(max_write_bytes) / 1024)
+
+        utils.exec_shell_cmd(
+            f"radosgw-admin ratelimit disable --ratelimit-scope=bucket --bucket={bucket_name}"
+        )
+        limset = utils.exec_shell_cmd(
+            f"radosgw-admin ratelimit set --ratelimit-scope=user --uid={user_name}"
+            + f" --max-read-ops={max_read_ops} --max-read-bytes={max_read_bytes}"
+            + f" --max-write-bytes={max_write_bytes} --max-write-ops={max_write_ops}"
+        )
+        log.info(f"Rate limits set on user {user_name}")
+        limenable = utils.exec_shell_cmd(
+            f"radosgw-admin ratelimit enable --ratelimit-scope=user --uid={user_name}"
+        )
+        limget = utils.exec_shell_cmd(
+            f"radosgw-admin ratelimit get --ratelimit-scope=user --uid={user_name}"
+        )
+        log.info(f"Rate limits enabled on bucket : {limget} ")
+
+        bucket_name2 = utils.gen_bucket_name_from_userid(user_name, rand_no=1)
+        bucket_name2 = bucket_name2.replace("'", "")
+        if config.version_enable:
+            s3cmd_reusable.create_versioned_bucket(
+                user_info, bucket_name2, ip_and_port, ssl
+            )
+        else:
+            s3cmd_reusable.create_bucket(bucket_name2, ssl)
+
+        # test the read and write ops limit
         log.info(f"Test the read and write ops limits")
         s3cmd_reusable.rate_limit_read(bucket_name2, max_read_ops, ssl)
 
         log.info(f"Sleeping for a minute to reset limits")
         sleep(61)
+
         s3cmd_reusable.rate_limit_write(bucket_name2, max_write_ops, ssl)
 
-    if config.test_ops.get("test_debt", False):
-        log.info("Test the rate limit debt feature ")
-        log.info("Write more than double the max write bytes")
-        log.info("Sleep for a minute to reset limits")
+        # sleep a minute to reset the ops limit
+        log.info(f"Sleeping for a minute to reset limits")
         sleep(61)
-        debt_limit = 2 * int(max_write_bytes_kb)
-        s3cmd_reusable.debt_ratelimit(bucket_name2, debt_limit, ssl)
 
-        # post a write above the set limit , debt is in effect
-        # further writes will fail until 2 minutes
+        # test the read and write data limit
+        log.info(f"Test the read and write data limits")
+        s3cmd_reusable.rate_limit_read(bucket_name2, max_read_bytes_kb, ssl)
+
+        log.info(f"Sleeping for a minute to reset limits")
+        sleep(61)
         s3cmd_reusable.rate_limit_write(bucket_name2, max_write_bytes_kb, ssl)
-        log.info("Sleep for 120 seconds for the debt to conclude")
-        sleep(121)
-        s3cmd_reusable.rate_limit_write(bucket_name2, max_write_bytes_kb, ssl)
+
+        if config.test_ops.get("daemon_add", False):
+            log.info("Add a RGW daemon to the existing configuration")
+            s3cmd_reusable.rgw_daemon_add(ssh_con)
+            log.info("Verify that the ratelimits should now be halved for each daemon")
+            max_read_ops = math.ceil(max_read_ops / 2)
+            max_write_ops = math.ceil(max_write_ops / 2)
+            # test the read and write ops limit
+            sleep(60)
+            log.info(f"Test the read and write ops limits")
+            s3cmd_reusable.rate_limit_read(bucket_name2, max_read_ops, ssl)
+
+            log.info(f"Sleeping for a minute to reset limits")
+            sleep(61)
+            s3cmd_reusable.rate_limit_write(bucket_name2, max_write_ops, ssl)
+
+        if config.test_ops.get("test_debt", False):
+            log.info("Test the rate limit debt feature ")
+            log.info("Write more than double the max write bytes")
+            log.info("Sleep for a minute to reset limits")
+            sleep(61)
+            debt_limit = 2 * int(max_write_bytes_kb)
+            s3cmd_reusable.debt_ratelimit(bucket_name2, debt_limit, ssl)
+
+            # post a write above the set limit , debt is in effect
+            # further writes will fail until 2 minutes
+            s3cmd_reusable.rate_limit_write(bucket_name2, max_write_bytes_kb, ssl)
+            log.info("Sleep for 120 seconds for the debt to conclude")
+            sleep(121)
+            s3cmd_reusable.rate_limit_write(bucket_name2, max_write_bytes_kb, ssl)
 
 
 if __name__ == "__main__":
