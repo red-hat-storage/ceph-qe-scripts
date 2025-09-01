a1d3eab7b3ebbc338edd54c88643219357c70702
Anuchaithra <anrao@redhat.com>
2025-09-01 16:39:42 +0530

    [closeloop][Tier-2]copy from secondary
    Signed-off-by: Anuchaithra <anrao@redhat.com>

--- rgw/v2/lib/s3cmd/auth.py
+++ rgw/v2/lib/s3cmd/auth.py
@@ -10,6 +10,8 @@ import sys
 from configparser import RawConfigParser
 from pathlib import Path
 
+import paramiko
+
 sys.path.append(os.path.abspath(os.path.join(__file__, "../../../../")))
 log = logging.getLogger()
 
@@ -73,7 +75,19 @@ def copy_to_home_directory():
     log.info("S3CMD config file .s3cfg got created")
 
 
-def do_auth(user_info, ip_and_port):
+def push_s3cfg_to_remote(ssh_remote_host):
+    # Copy file via SFTP
+    sftp = ssh_remote_host.open_sftp()
+    remote_path = "/home/cephuser/.s3cfg"
+    sftp.put(f"{root_path}/s3cfg", remote_path)
+    sftp.close()
+
+    # Verify on remote
+    _, stdout, _ = ssh_remote_host.exec_command(f"ls -l {remote_path}")
+    log.info(stdout.read().decode())
+
+
+def do_auth(user_info, ip_and_port, ssh_remote_host=None):
     """
     Performs steps for s3 authentication
     Args:
@@ -85,3 +99,5 @@ def do_auth(user_info, ip_and_port):
     copy_to_home_directory()
     log.info("S3CMD Version:")
     utils.exec_shell_cmd(f"{home_path}/venv/bin/s3cmd --version")
+    if ssh_remote_host:  # push config to remote if host given
+        push_s3cfg_to_remote(ssh_remote_host)
