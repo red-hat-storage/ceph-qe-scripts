8a90333889575e3bfbacb4f81adb7bdfba4c8c28
MadhaviKasturi <mkasturi@redhat.com>
2026-04-06 15:34:04 +0000

    update: use ibmc gklm server as default
    Signed-off-by: MadhaviKasturi <mkasturi@redhat.com>

--- rgw/v2/tests/s3_swift/test_sse_s3_kms_with_vault.py
+++ rgw/v2/tests/s3_swift/test_sse_s3_kms_with_vault.py
@@ -73,10 +73,13 @@ def test_exec(config, ssh_con):
     if rgw_crypt_s3_kms_backend == "kmip":
         out = utils.exec_shell_cmd("ceph config get client.rgw rgw_crypt_kmip_addr")
         rgw_crypt_kmip_addr = out.strip()
-        if rgw_crypt_kmip_addr == "10.245.64.16:5696":
-            config.test_ops[
-                "encrypt_decrypt_key"
-            ] = "ibm000ee6a57000000000"  # use corresponding key id for ibmc env
+        config.test_ops["encrypt_decrypt_key"] = (
+            "gkl005686891000000000"  # default to using gklm key from IBMC env
+        )
+        if rgw_crypt_kmip_addr == "10.0.215.90:5696":
+            config.test_ops["encrypt_decrypt_key"] = (
+                "gkl0047c159c00000000a"  # use corresponding gklm key id for rhosd env
+            )
     # create user
     all_users_info = s3lib.create_users(config.user_count)
     for each_user in all_users_info:
